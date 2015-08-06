14eb2a8950d54b2c94cd5ae4b409047c6b9b0a08
Jewel Farazi <jeweljf@gmail.com>
2015-08-06 07:47:51 +0600

    first page content added

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+$( document ).on( "pagecreate", "#home", function() {
+    $( document ).on( "swipeleft swiperight", "#home", function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swipeleft" ) {
+                $( "#right-panel" ).panel( "open" );
+            } else if ( e.type === "swiperight" ) {
+                $( "#left-panel" ).panel( "open" );
+            }
+        }
+    });
+});
